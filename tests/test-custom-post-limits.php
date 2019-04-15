d1df9327eeacd20d80c1c2c0072abb0636f2e343
Scott Reilly <scott@coffee2code.com>
2019-04-14 21:05:09 -0600

    Update plugin framework to 049
    * 049:
    * Correct last arg in call to `add_settings_field()` to be an array
    * Wrap help text for settings in `label` instead of `p`
    * Only use `label` for help text for checkboxes, otherwise use `p`
    * Ensure a `textarea` displays as a block to prevent orphaning of subsequent help text
    * Note compatibility through WP 5.1+
    * Update copyright date (2019)

--- tests/test-custom-post-limits.php
+++ tests/test-custom-post-limits.php
@@ -82,11 +82,11 @@ class Custom_Post_Limits_Test extends WP_UnitTestCase {
 	}
 
 	public function test_plugin_framework_class_name() {
-		$this->assertTrue( class_exists( 'c2c_CustomPostLimits_Plugin_048' ) );
+		$this->assertTrue( class_exists( 'c2c_CustomPostLimits_Plugin_049' ) );
 	}
 
 	public function test_plugin_framework_version() {
-		$this->assertEquals( '048', c2c_CustomPostLimits::get_instance()->c2c_plugin_version() );
+		$this->assertEquals( '049', c2c_CustomPostLimits::get_instance()->c2c_plugin_version() );
 	}
 
 	public function test_version() {
